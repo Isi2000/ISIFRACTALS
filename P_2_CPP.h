6577adbb4681a67c9692035eaef8d5244ddf6c1d
Isacco Faglioni <isaccofaglioni2000@gmail.com>
2023-06-28 16:47:47 +0200

    data in a handy fomat for zooming

--- P_2_CPP.h
+++ P_2_CPP.h
@@ -2,10 +2,14 @@
 #include <vector>
 #include <complex>
 #include <fstream>
+#include <cmath>
+#include <string>
+
+const double e = 2.71828182845904523536;
 
 int num_iter(std::complex<double> z0, std::complex<double> c, int max_iter, double thresh = 4)
 {
-  //calculates the number of iterations for each pixel
+    //calculates the number of iterations for each pixel
     std::complex<double> zn = z0;
     int it = 0;
     while ((std::norm(zn) < thresh) && it < max_iter) {
@@ -14,6 +18,7 @@ int num_iter(std::complex<double> z0, std::complex<double> c, int max_iter, doub
     }
     return it;
 }
+
 class BOARD {
     int dim;
     std::vector<double> board;
@@ -21,25 +26,25 @@ class BOARD {
 public:
     BOARD(int dim) : dim(dim), board(dim * dim, 1.0) { }
 
-    void coloring_board() {
-      double s_f = 0.15;
-      //std::complex<double> s(-0.355, 0.355);
-      //assigns to each pixel the color corresponding to num_iter
-        double z_real_bound = 2.48* s_f / (this->dim - 1);
-        double z_im_bound = 2.26 * s_f /(this->dim - 1);
+    void coloring_board(double scaling_factor) {
+        std::complex<double> s(-0.3, 0.05);
+        //assigns to each pixel the color corresponding to num_iter
+        double z_real_bound = 2.48 * scaling_factor / (this->dim - 1);
+        double z_im_bound = 2.26 * scaling_factor / (this->dim - 1);
 
         for (int x = 0; x < this->dim; ++x) {
             for (int y = 0; y < this->dim; ++y) {
-	      double real = x * z_real_bound - 5.1*s_f;
-	      double im = y * z_im_bound - 0.413*s_f;
+                double real = x * z_real_bound - e/7 * scaling_factor;
+                double im = y * z_im_bound - e/20 * scaling_factor;
                 int number_iterations = num_iter(0, std::complex<double>(real, im), 100);
-                this->board[y * this->dim + x] = 1 - number_iterations/100;
+                this->board[y * this->dim + x] = 1 - number_iterations / 100;
             }
         }
     }
 
-  void save_to_file() {
-        std::ofstream outfile("output.txt");
+    void save_to_file(double scaling_factor) {
+        std::string filename = "output_scaling_" + std::to_string(scaling_factor) + ".txt";
+        std::ofstream outfile(filename);
 
         for (int i = 0; i < this->dim; i++) {
             for (int j = 0; j < this->dim; j++) {
@@ -51,3 +56,4 @@ public:
         outfile.close();
     }
 };
+
--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include"P_2_CPP.h"
 
 
-using namespace std;
-int main()
-{
-    BOARD a(10000);
-    a.coloring_board();
-    a.save_to_file();
+
+int main() {
+    int dim = 500;
+
+    for (double scaling_factor = 2.0; scaling_factor >= 0.2; scaling_factor -= 0.2) {
+        BOARD board(dim);
+        board.coloring_board(scaling_factor);
+        board.save_to_file(scaling_factor);
+    }
+
     return 0;
-
-
-  
-};
+}
