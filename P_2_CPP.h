559159848a8e2011e45904aa5b21a0f58db1e767
Isacco Faglioni <isaccofaglioni2000@gmail.com>
2023-06-28 03:10:44 +0200

    goofing around with zoom params, in order to do this correctly u need to calculate stuff by hand

--- P_2_CPP.h
+++ P_2_CPP.h
@@ -14,7 +14,6 @@ int num_iter(std::complex<double> z0, std::complex<double> c, int max_iter, doub
     }
     return it;
 }
-
 class BOARD {
     int dim;
     std::vector<double> board;
@@ -23,17 +22,18 @@ public:
     BOARD(int dim) : dim(dim), board(dim * dim, 1.0) { }
 
     void coloring_board() {
-      double s_f = 0.25;
+      double s_f = 0.15;
+      //std::complex<double> s(-0.355, 0.355);
       //assigns to each pixel the color corresponding to num_iter
         double z_real_bound = 2.48* s_f / (this->dim - 1);
         double z_im_bound = 2.26 * s_f /(this->dim - 1);
 
         for (int x = 0; x < this->dim; ++x) {
             for (int y = 0; y < this->dim; ++y) {
-	      double real = x * z_real_bound - 3.5*s_f;
-                double im = y * z_im_bound - 1.13*s_f;
-                int number_iterations = num_iter(0, std::complex<double>(real, im), 500);
-                this->board[y * this->dim + x] = 1 - number_iterations / 500;
+	      double real = x * z_real_bound - 5.1*s_f;
+	      double im = y * z_im_bound - 0.413*s_f;
+                int number_iterations = num_iter(0, std::complex<double>(real, im), 100);
+                this->board[y * this->dim + x] = 1 - number_iterations/100;
             }
         }
     }
